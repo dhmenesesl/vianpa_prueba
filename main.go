28094edeccc738ef853534b27ec57734052b84c1
dhmenesesl <dhmenesesl@gmail.com>
2017-05-20 02:03:37 -0500

    Pidiendo parametro ciudad de origen

--- main.go
+++ main.go
@@ -36,6 +36,8 @@ func main() {
 	}
 	defer session.Close()
 
+	//cc conexion
+
 	cc := session.DB("vianca-db").C("vuelo")
 	//err = cc.Insert(&flight{"001", "Medellin", "Bogota", "100000", "USD"},
 	//	&flight{"002", "Medellin", "Barranquilla", "150000", "USD"})
@@ -46,15 +48,18 @@ func main() {
 	//result := flight{}
 	var results []flight
 
-	err = cc.Find(bson.M{"origin": "Medellin"}).All(&results)
+	router.GET("/search/origin/:ciudadori", func(c *gin.Context) {
+	
+	ciudadori := c.Param("ciudadori")
+	err = cc.Find(bson.M{"origin": ciudadori}).All(&results)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"code": "12345", "name": "Vianca Airlines", "thumbnail":"https://image.freepik.com/icones-gratuites/avion-noir_318-31722.jpg", "results": results})
 	})
 
 	router.Run(":" + port)
+	
 }
